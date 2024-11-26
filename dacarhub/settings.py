698cb12069cfb102b97cd8ed0722ecba44a7500b
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-11-27 00:05:21 +0100

    fixed

--- dacarhub/settings.py
+++ dacarhub/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q_+!9e#5duxl-)sm^+%^6+b4+#d2(n3og&fmjhk!m@0!6%95b$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dacarhub.vercel.app', '127.0.0.1', 'localhost']
 
